f79d34055e5c4082fd6b8ae6443d773f5d61a31c
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-12 14:26:42 -0600

    two algorithm improved

--- two/index.js
+++ two/index.js
@@ -1,9 +1,10 @@
-function move(x, y) {
+function move(x, y, obj = {}) {
   if (x === 1 || y === 1) return 1
+  if (`${x} ${y}` in obj) return obj[`${x} ${y}`]
 
-  return move(x - 1, y) + move(x, y - 1)
+  return obj[`${x} ${y}`] = move(x - 1, y, obj) + move(x, y - 1, obj)
 }
 
 console.log(
-  move(6, 2)
+  move(3, 3)
 )
