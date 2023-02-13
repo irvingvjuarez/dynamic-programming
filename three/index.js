e92151404536f2ccabca4a6c099a38357c165d68
Irving Juárez <irvingjuarez274@gmail.com>
2023-02-12 20:32:12 -0600

    Three problem already solved, but not optimazed yet

--- three/index.js
+++ three/index.js
@@ -1,3 +1,30 @@
 function canSum(targetSum, numbers) {
-  return false
+  if (targetSum === 0) return true
+  if (targetSum < 0) return false
+
+  const results = []
+  numbers = Array.from(new Set(numbers)).sort((a, b) => {
+    let res = 0
+    if (a > b) res = 1
+    if (a < b) res = -1
+    return res
+  })
+
+  numbers.forEach(number => {
+    results.push(canSum(targetSum - number, numbers))
+  })
+
+  return results.some(result => result)
 }
+
+console.log(
+  canSum(6, [3, 2, 5, 3]) // true
+)
+
+console.log(
+  canSum(7, [5, 3, 4, 7]) // true
+)
+
+console.log(
+  canSum(7, [2, 4]) // false
+)
